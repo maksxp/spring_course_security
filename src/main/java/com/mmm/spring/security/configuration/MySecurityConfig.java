20566828e4d21e628443608ca7b96e73000bb856
Maksym <maksym.murza@gmail.com>
2021-08-16 11:49:23 +0300

    completed

--- src/main/java/com/mmm/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/mmm/spring/security/configuration/MySecurityConfig.java
@@ -1,22 +1,46 @@
 package com.mmm.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
 
-        auth.inMemoryAuthentication()
-                .withUser(userBuilder.username("test1").password("test1").roles("EMPLOYEE"))
-                .withUser(userBuilder.username("test2").password("test2").roles("HR"))
-                .withUser(userBuilder.username("test1").password("test1").roles("HR", "MANAGER"));
+        //lines below use for database Authentication
+        auth.jdbcAuthentication()
+                .dataSource(dataSource);
+
+//        //lines below use for inMemoryAuthentication
+//        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.username("test1").password("test1").roles("EMPLOYEE"))
+//                .withUser(userBuilder.username("test2").password("test2").roles("HR"))
+//                .withUser(userBuilder.username("test3").password("test3").roles("HR", "MANAGER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http
+                .authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("MANAGER")
+                .and()
+                .formLogin()
+                .permitAll();
 
     }
 }
---
+++ src/main/webapp/WEB-INF/view/view_for_managers.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<h3>Here you can view performance of all employees</h3>
+
+</body>
+
+
+</html>
