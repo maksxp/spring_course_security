efb04e11069a08ac7f8827a0f6677a1e1924debf
Maksym <maksym.murza@gmail.com>
2021-08-16 10:32:08 +0300

    configuration with inMemoryAuthentication completed

---
+++ src/main/java/com/mmm/spring/security/configuration/MyConfig.java
@@ -0,0 +1,23 @@
+package com.mmm.spring.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan (basePackages = "com.mmm.spring.security")
+@EnableWebMvc
+public class MyConfig {
+
+    @Bean
+    public ViewResolver viewResolver () {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
